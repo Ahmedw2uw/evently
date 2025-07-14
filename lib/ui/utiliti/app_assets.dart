8220161de1b6b6192565940a15e1b29fd9175ad3
Ahmed Ashraf <your-email@example.com>
2025-07-14 15:31:18 +0300

    initial thing in evently project create thie app colors class, and initial usage images in thie app.

---
+++ lib/ui/utiliti/app_assets.dart
@@ -0,0 +1,13 @@
+class AppAssets {
+  //! backgrounds
+  static const String logo="asset/images/logo.png";
+  static const String forgetPassword="asset/images/forget_password.png";
+
+
+  //! icons
+  static const String imailIcon="asset/images/email_icon.png";
+  static const String passwordIcon="asset/images/password_icon.png";
+  static const String personIcon="asset/images/person_ic.png";
+
+
+}
---
+++ lib/ui/utiliti/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static const Color purple = Color(0xff5669FF);
+  static const Color white = Color(0xfff0f0f0);
+  static const Color black = Color(0xff000000);
+  static const Color blue = Color(0xff0052B4);
+  static const Color green = Color(0xff2BA24C);
+  static const Color babyBlue = Color(0xff3B7DED);
+  static const Color red = Color(0xffE43E2B);
+  static const Color yellow = Color(0xffF0B501);
+  static const Color orange = Color(0xffFF9811);
+  static const Color gray = Color(0xff7B7B7B);
+}
